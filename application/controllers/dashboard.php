485d112f6992909745e3113b751d1f81a2414ea7
DevTarmidi <dede.tarmidi@gmail.com>
2017-12-13 20:46:34 +0700

    litle commit

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -5,8 +5,17 @@ class dashboard extends CI_Controller {
 	public function index()
 	{
 		$data['menu']=4;
+		$udate['status'] = false;
 		$this->load->view('header', $data);
-		$this->load->view('dashboard');
+		$this->load->view('dashboard', $udate);
+		$this->load->view('footer');
+	}
+	public function update()
+	{
+		$data['menu']=4;
+		$udate['status'] = true;
+		$this->load->view('header', $data);
+		$this->load->view('dashboard', $udate);
 		$this->load->view('footer');
 	}
 }
